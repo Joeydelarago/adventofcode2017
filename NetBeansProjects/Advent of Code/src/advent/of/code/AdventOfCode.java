c608b952a581cfda89ee70c0a59728644d4a853b
Joey <joeydelarago@rocketmail.com>
2018-03-21 17:51:32 +0000

    Day 1 of Advent of Code in Java.

---
+++ NetBeansProjects/Advent of Code/src/advent/of/code/AdventOfCode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package advent.of.code;
+
+/**
+ *
+ * @author joey
+ */
+
+public class AdventOfCode {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AdventOfCode1 advent1 = new AdventOfCode1();
+        advent1.advent1();
+        advent1.advent2();
+    }
+    
+}
